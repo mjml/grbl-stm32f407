59d2e628e9e608e42c7cb7a7cc4ed5a4d90af9bf
Michael Joya <mike@michaeljoya.com>
2021-05-13 14:37:41 -0700

    when asked to compile a header file, it will rename it to the corresponding .c file and compile that as a convenience

--- compile.sh
+++ compile.sh
@@ -8,12 +8,14 @@ srcfile=$1
 if [[ $srcfile =~ ^grbl ]]; then
 		srcfile=${srcfile#*grbl/}
 fi
-if [[ ! $srcfile =~ .*\.c ]]; then
+if [[ $srcfile =~ .*\.c ]]; then
+		tgtfile=build/${srcfile%.c}.o
+elif [[ $srcfile =~ .*\.h ]]; then
+		tgtfile=build/${srcfile%.h}.o
+else
 		echo "$srcfile is not a valid source file!"
 		exit
 fi
-
-tgtfile=build/${srcfile%.c}.o
 
 make -j -f stm32.Makefile $tgtfile
 
